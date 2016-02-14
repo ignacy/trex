91b396bf49ce8f009ff9f3aa6e30124a7da7548d
Michał Darda <michaldarda@gmail.com>
2016-02-14 21:14:51 +0100

    Convert dir structure to umbrella app

---
+++ apps/trex_server/mix.exs
@@ -0,0 +1,25 @@
+defmodule TrexServer.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :trex_server,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps
+    ]
+  end
+
+  def application do
+    [
+      applications: [:logger],
+      mod: {TrexServer, []}
+    ]
+  end
+
+  defp deps do
+    []
+  end
+end
--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,3 @@
 use Mix.Config
+
+import_config "../apps/*/config/config.exs"
