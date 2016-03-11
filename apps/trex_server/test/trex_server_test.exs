d72f47f5506becf822a5e95f884e7d4055960a1a
Ignacy Moryc <imoryc@gmail.com>
2016-03-11 14:29:01 +0100

    Oups I broke a test

--- apps/trex_server/test/trex_server_test.exs
+++ apps/trex_server/test/trex_server_test.exs
@@ -15,7 +15,7 @@ defmodule TrexServerTest do
   test "server interaction", %{socket: socket} do
     assert send_and_recv(socket, "PING\r\n") == "PONG\r\n"
 
-    assert send_and_recv(socket, "NOT_IMPLEMENTED\r\n") == "UNKNOWN COMMAND\r\n"
+    assert send_and_recv(socket, "NOT_IMPLEMENTED\r\n") == "UNKNOWN_COMMAND\r\n"
 
     assert send_and_recv(socket, "SET\tSOMEKEY\tSOMEVALUE\r\n") == "OK\r\n"
     assert send_and_recv(socket, "GET\tSOMEKEY\r\n") == "SOMEVALUE\r\n"
