ce28aa6c4373b1d4e5656b8b011488cec1434b73
Ignacy Moryc <imoryc@gmail.com>
2016-03-18 11:12:49 +0100

    Move Connection handling code out of server

---
+++ lib/trex/connection_handler.ex
@@ -0,0 +1,43 @@
+defmodule Trex.ConnectionHandler do
+  alias Trex.{CommandEvaluator}
+  require Logger
+
+  def serve(socket) do
+    msg = case read_line(socket) do
+            {:ok, data} ->
+              Logger.info "Processing #{data}"
+              CommandEvaluator.evaluate(data)
+            err -> err
+          end
+
+    write_line(socket, msg)
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    :gen_tcp.recv(socket, 0)
+  end
+
+  defp write_line(socket, {:ok, command}) do
+    :gen_tcp.send(socket, to_string(command) <> "\r\n")
+  end
+
+  defp write_line(socket, {:error, :unknown_command}) do
+    :gen_tcp.send(socket, "UNKNOWN_COMMAND\r\n")
+  end
+
+  defp write_line(_socket, {:error, :closed}) do
+    exit(:shutdown)
+  end
+
+  defp write_line(socket, {:error, error}) do
+    :gen_tcp.send(socket, "ERROR\r\n")
+    exit(error)
+  end
+
+  defp write_line(socket, message) do
+    Logger.error "Unexpected command result #{message}"
+    :gen_tcp.send(socket, "#{inspect message}\r\n")
+    exit(:shutdown)
+  end
+end
--- lib/trex/server.ex
+++ lib/trex/server.ex
@@ -1,5 +1,5 @@
 defmodule Trex.Server do
-  alias Trex.{CommandEvaluator, Server.TaskSupervisor}
+  alias Trex.{ConnectionHandler, Server.TaskSupervisor}
   require Logger
 
   @port System.get_env("TREX_PORT") || 4040
@@ -9,55 +9,15 @@ defmodule Trex.Server do
     [:binary, packet: :line, active: false, reuseaddr: true])
     Logger.info "Accepting connections on port #{@port}"
 
-    loop_acceptor(socket)
+    loop(socket)
   end
 
-  defp loop_acceptor(socket) do
+  defp loop(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
     {:ok, pid} = Task.Supervisor.start_child TaskSupervisor, fn ->
-      serve(client)
+      ConnectionHandler.serve(client)
     end
     :ok = :gen_tcp.controlling_process(client, pid)
-    loop_acceptor(socket)
-  end
-
-  defp serve(socket) do
-    msg =
-    case read_line(socket) do
-      {:ok, data} ->
-        Logger.info "Processing #{data}"
-        CommandEvaluator.evaluate(data)
-      err -> err
-    end
-
-    write_line(socket, msg)
-    serve(socket)
-  end
-
-  defp read_line(socket) do
-    :gen_tcp.recv(socket, 0)
-  end
-
-  defp write_line(socket, {:ok, command}) do
-    :gen_tcp.send(socket, to_string(command) <> "\r\n")
-  end
-
-  defp write_line(socket, {:error, :unknown_command}) do
-    :gen_tcp.send(socket, "UNKNOWN_COMMAND\r\n")
-  end
-
-  defp write_line(_socket, {:error, :closed}) do
-    exit(:shutdown)
-  end
-
-  defp write_line(socket, {:error, error}) do
-    :gen_tcp.send(socket, "ERROR\r\n")
-    exit(error)
-  end
-
-  defp write_line(socket, message) do
-    Logger.error "Unexpected command result #{message}"
-    :gen_tcp.send(socket, "#{inspect message}\r\n")
-    exit(:shutdown)
+    loop(socket)
   end
 end
