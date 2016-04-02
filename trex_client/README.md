669d912e2dd56ab368f0479ffa854651a34b1130
Michał Darda <michaldarda@gmail.com>
2016-04-02 02:01:30 +0200

    First working implementation

--- trex_client/README.md
+++ trex_client/README.md
@@ -1,20 +1,4 @@
 # TrexClient
 
-**TODO: Add description**
-
-## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
-
-  1. Add trex_client to your list of dependencies in `mix.exs`:
-
-        def deps do
-          [{:trex_client, "~> 0.0.1"}]
-        end
-
-  2. Ensure trex_client is started before your application:
-
-        def application do
-          [applications: [:trex_client]]
-        end
+Client for trex
 
--- trex_client/lib/trex_client.ex
+++ trex_client/lib/trex_client.ex
@@ -1,2 +1,46 @@
+defmodule TrexServerClient do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, nil)
+  end
+
+  def init(_state) do
+    opts = [:binary, active: false]
+    :gen_tcp.connect('127.0.0.1', 4040, opts)
+  end
+
+  def command(pid, command) do
+    IO.inspect command
+
+    GenServer.call(pid, command)
+  end
+
+  def handle_call(command, from, socket) do
+    :ok = :gen_tcp.send(socket, command)
+    {:ok, msg} = :gen_tcp.recv(socket, 0)
+
+    {:reply, msg, socket}
+  end
+
+  def cmd(command) do
+    "#{command}\r\n"
+  end
+end
+
 defmodule TrexClient do
+  def main(args) do
+    {:ok, pid} = TrexServerClient.start_link
+
+    loop(pid)
+  end
+
+  def loop(pid) do
+    command = IO.gets("127.0.0.1:4040> ")
+
+    reply = TrexServerClient.command(pid, String.replace(command, " ", "\t"))
+    IO.puts reply
+
+    loop(pid)
+  end
 end
