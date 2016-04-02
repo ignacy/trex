84c4319ef9902e692fb62231ea5dc204a7afe49d
Michał Darda <michaldarda@gmail.com>
2016-04-03 00:32:41 +0200

    Rename project and split into separate files

---
+++ trex_cli/lib/trex_cli.ex
@@ -0,0 +1,26 @@
+defmodule TrexCLI do
+  def main(args) do
+    {options, _, _} = OptionParser.parse(args,
+    switches: [host: :string, port: :integer],
+    aliases: [h: :host, p: :port])
+
+    host = Keyword.get(options, :host, '127.0.0.1')
+    port = Keyword.get(options, :port, 4040)
+
+    TrexCLI.Supervisor.start_link(host, port)
+
+    loop(host, port)
+  end
+
+  def loop(host, port) do
+    command = IO.gets("#{host}:#{port}> ")
+
+    reply = command
+    |> String.replace(" ", "\t")
+    |> TrexCLI.Client.command
+
+    IO.puts reply
+
+    loop(host, port)
+  end
+end
--- trex_cli/mix.exs
+++ trex_cli/mix.exs
@@ -1,14 +1,14 @@
-defmodule TrexClient.Mixfile do
+defmodule TrexCLI.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :trex_client,
+    [app: :trex_cli,
      version: "0.0.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps,
-     escript: [main_module: TrexClient]]
+     escript: [main_module: TrexCLI]]
   end
 
   def application do
