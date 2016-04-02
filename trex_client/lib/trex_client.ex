4820d71fadbdc359b1610ab1d6a0a34451302ad9
Michał Darda <michaldarda@gmail.com>
2016-04-02 03:23:25 +0200

    Another process for long running prompt

--- trex_client/lib/trex_client.ex
+++ trex_client/lib/trex_client.ex
@@ -2,23 +2,53 @@ defmodule TrexServerClient do
   use GenServer
 
   def start_link(host, port) do
+    IO.inspect("try to restart")
     GenServer.start_link(__MODULE__, {host, port}, [name: __MODULE__])
   end
 
   def init({host, port}) do
+    IO.inspect("try to restart")
+    Process.flag(:trap_exit, true)
+
     opts = [:binary, active: false]
     :gen_tcp.connect(host, port, opts)
   end
 
   def command(command) do
-    GenServer.call(__MODULE__, command)
+    case GenServer.call(__MODULE__, command) do
+      reply -> reply
+      otherwise -> "Connection error"
+    end
   end
 
   def handle_call(command, _from, socket) do
-    :ok = :gen_tcp.send(socket, command)
-    {:ok, msg} = :gen_tcp.recv(socket, 0)
+    :gen_tcp.send(socket, command)
 
-    {:reply, msg, socket}
+    case :gen_tcp.recv(socket, 0) do
+      {:ok, msg } -> {:reply, msg, socket}
+      {:error, reason } -> {:stop, :normal}
+    end
+  end
+
+  def terminate(_reason, state) do
+    IO.inspect "closing connection"
+
+    :gen_tcp.close(state)
+  end
+end
+
+defmodule TrexPrompt do
+  def start_link do
+    IO.inspect "Starting.."
+    loop
+  end
+
+  defp loop do
+    command = IO.gets("127.0.0.1:4040> ")
+
+    reply = TrexServerClient.command(String.replace(command, " ", "\t"))
+
+    loop
   end
 end
 
@@ -30,19 +60,11 @@ defmodule TrexClient do
   def run(mod) do
     import Supervisor.Spec
 
-    children = [worker(TrexServerClient, ['127.0.0.1', 4040])]
+    children = [
+      worker(TrexServerClient, ['127.0.0.1', 4040]),
+      worker(TrexPrompt, [])
+    ]
 
-    Supervisor.start_link(children, strategy: :one_for_one)
-
-    loop
-  end
-
-  def loop do
-    command = IO.gets("127.0.0.1:4040> ")
-
-    reply = TrexServerClient.command(String.replace(command, " ", "\t"))
-    IO.puts reply
-
-    loop
+    Supervisor.start_link(children, strategy: :one_for_one, restart: :permament)
   end
 end
